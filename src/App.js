15996dba3a34219b01ab5dad188d12c7db260584
Julian Mahecha <jmahecha@belatrixsf.com>
2018-10-30 14:53:47 -0500

    burgerBuilder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from './components/Layout/Layout';
+import BurguerBuilder from './containers/BurgerBuilder/BurguerBuilder';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Layout>
+          <BurguerBuilder></BurguerBuilder>
+        </Layout>
       </div>
     );
   }
