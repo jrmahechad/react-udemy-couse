c1484a62deae01f0505341135083d4758087036d
DESKTOP-A4RCM1V\jrmah <jrmahecha88@gmail.com>
2018-11-02 17:35:46 -0500

    basic http use with axios, GET/POST/DELETE

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +7,56 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log('[GET] all', response);
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(p => {
+          return {
+            ...p,
+            author: 'Julian'
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ error: true });
+      });
+  }
+
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    let posts = <p style={{ textAlign: 'center' }}>Something went wrong!!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(p => {
+        return <Post key={p.id} title={p.title} author={p.author} clicked={() => this.postSelectedHandler(p.id)} />;
+      });
     }
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
