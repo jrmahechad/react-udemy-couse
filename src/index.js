789e653c4aef5128518166b473a2ed94042e95ad
Julian Mahecha <jmahecha@belatrixsf.com>
2018-11-06 10:59:49 -0500

    interceptos and axios instances

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL='https://jsonplaceholder.typicode.com'
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type']= 'application/json';
+
+//const myInterceptor = axios.interceptors.request.use(request=>{
+axios.interceptors.request.use(request=>{
+  console.log('[INTERCEPTOR] Request',request);
+  return request;
+}, error => {
+  console.log('[INTERCEPTOR] Error',error);
+  return Promise.reject(error);
+});
+
+// Remove interceptor
+// axios.interceptors.request.eject(myInterceptor);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
