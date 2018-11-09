ef2866008874f16e07f481ab9041da5288f53c1e
DESKTOP-A4RCM1V\jrmah <jrmahecha88@gmail.com>
2018-11-09 13:40:13 -0500

    finish

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import  queryString  from "querystring";
 
 class Course extends Component {
+    
     render () {
+        const values = queryString.parse(this.props.location.search.replace('?',''))
+        console.log(this.props);
+        console.log(values);
+        
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{values.title}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
