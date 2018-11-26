0fdd1ce8889a26f567f2dce7c48d865001c9395d
Julian Mahecha <jmahecha@belatrixsf.com>
2018-11-26 14:19:21 -0500

    animations start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>React Animations</h1>
+        <Modal />
+        <Backdrop />
+        <button className="Button">Open Modal</button>
+        <h3>Animating Lists</h3>
+        <List />
       </div>
     );
   }
